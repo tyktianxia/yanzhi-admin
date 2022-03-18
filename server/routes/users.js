f75e1f0f27cfd95715ce3365a651e2c3c71ba5a2
tanyukang <tanyukang888@163.com>
2022-03-18 18:02:35 +0800

    1

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,4 +1,5 @@
 var express = require("express");
+var Mock = require("mockjs");
 var router = express.Router();
 var utiltest = require("../utils/index");
 
@@ -10,8 +11,16 @@ router.get("/", function (req, res, next) {
 
 // 获取用户列表
 router.get("/userList", function (req, res, next) {
-  res.send(utiltest.normalRes());
+  var data = Mock.mock({
+    // 属性 list 的值是一个数组，其中含有 1 到 10 个元素
+    "list|1-10": [
+      {
+        // 属性 id 是一个自增数，起始值为 1，每次增 1
+        "id|+1": 1,
+      },
+    ],
+  });
+  res.send(utiltest.normalRes(data));
 });
-
 
 module.exports = router;
--- server/utils/index.js
+++ server/utils/index.js
@@ -1,17 +1,10 @@
-
-
 module.exports = {
-  normalRes: (data = {}, code = "00000", msg = "操作成功！") => {
+  normalRes: (data, code = "00000", msg = "SUCCESS") => {
     return {
       code: "00000",
-      data: {
-        code: code,
-        data: data,
-        message: msg,
-        msg,
-      },
-      message: "",
-      msg: "",
+      data: data,
+      message: "SUCCESS",
+      msg: "SUCCESS",
     };
   },
   abnormalRes: (code = "50001", msg = "操作失败！") => {
@@ -22,4 +15,25 @@ module.exports = {
       msg,
     };
   },
+  // normalRes: (data = {}, code = "00000", msg = "操作成功！") => {
+  //   return {
+  //     code: "00000",
+  //     data: {
+  //       code: code,
+  //       data: data,
+  //       message: msg,
+  //       msg,
+  //     },
+  //     message: "",
+  //     msg: "",
+  //   };
+  // },
+  // abnormalRes: (code = "50001", msg = "操作失败！") => {
+  //   return {
+  //     code,
+  //     data: null,
+  //     message: msg,
+  //     msg,
+  //   };
+  // },
 };
