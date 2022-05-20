26ebabd3a50eacfcd28bdc6daa471558c13cc7fe
tanyukang <tanyukang888@163.com>
2022-05-20 17:03:40 +0800

    1

---
+++ src/views/myTest/component/child3.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import { ElButton } from "element-plus";
+import { h } from "vue";
+let props = defineProps({
+  testHtml: String,
+});
+console.log(props.testHtml);
+</script>
+
+<script lang="ts">
+export default {
+  render() {
+    return h("div", {
+      innerHTML: this.testHtml,
+    });
+  },
+  // mounted(){
+  //   console.log(props.testHtml)
+  // },
+  props:{
+    "testHtml":{
+      type:String
+    }
+  },
+  methods: {
+    testHtmlClick() {
+      console.log("testHtmlClick");
+    },
+  },
+};
+</script>
