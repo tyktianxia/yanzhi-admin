a76b9569b2916c1cc128f476a555edd7d32221ab
yanzhi <tyktianxia@gmail.com>
2022-05-25 23:38:36 +0800

    登录优化

--- src/views/login.vue
+++ src/views/login.vue
@@ -2,17 +2,15 @@
 
   <div class="form_contain">
     <div class="yz_form">
-      <ElForm ref="loginFormRef" :model="ruleForm" :rules="rules">
-        <p class="yz_form_title">Login Page</p>
+      <ElForm ref="loginFormRef" :model="form" :rules="rules">
+        <p ref="testRef" class="yz_form_title">Login Page</p>
         <ElFormItem prop="username">
-          <el-input class="yz_form_input" v-model="ruleForm.username" placeholder="请输入用户名" :prefix-icon="User"
-            clearable />
+          <el-input class="yz_form_input" v-model="form.username" placeholder="请输入用户名" :prefix-icon="User" clearable />
         </ElFormItem>
         <ElFormItem prop="passward">
-          <el-input class="yz_form_input" v-model="ruleForm.passward" placeholder="请输入密码" :prefix-icon="Lock"
-            clearable />
+          <el-input class="yz_form_input" v-model="form.passward" placeholder="请输入密码" :prefix-icon="Lock" clearable />
         </ElFormItem>
-        <ElButton type="primary" class="yz_form_button" @click="toLogin">登录</ElButton>
+        <ElButton type="primary" class="yz_form_button" @click="toLogin" :loading="btnLoading">登录</ElButton>
       </ElForm>
     </div>
   </div>
@@ -29,8 +27,9 @@ import { ref, reactive } from "vue";
 let $router = useRouter();
 const CommonStore = Common();
 
-const loginFormRef = ref < FormInstance > ()
-const ruleForm = reactive({
+const btnLoading = ref(false)
+const loginFormRef = ref(null)
+const form = reactive({
   username: "",
   passward: ""
 })
@@ -44,22 +43,24 @@ const rules = reactive({
 })
 
 let toLogin = async () => {
-  // await loginFormRef.validate((valid, fields) => {
-  //   if (valid) {
-  //     console.log('submit!',fields)
-  //   } else {
-  //     console.log('error submit!', fields)
-  //   }
-  // })
-
-  CommonStore.setUserInfo({ tokenStr: 46456465 });
-  $router.push({
-    path: "/home",
-    query: {
-      id: "456456456645",
-      tokenStr: "yanzhihaoshuai",
-    },
-  });
+  // fields 错误信息
+  await loginFormRef.value.validate((valid, fields) => {
+    if (valid) {
+      console.log('校验成功，去登录!', fields)
+      btnLoading.value = true;
+      CommonStore.setUserInfo({ tokenStr: 46456465 });
+      $router.push({
+        path: "/home",
+        query: {
+          id: "456456456645",
+          tokenStr: "yanzhihaoshuai",
+        },
+      });
+    } else {
+      console.log('校验失败!', fields)
+    }
+  })
+  btnLoading.value = false;
 };
 
 // let setData = (str: string) => {
