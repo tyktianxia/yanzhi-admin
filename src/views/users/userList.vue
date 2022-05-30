1a0b07baf46008da1f9825930873d0d3702c28e3
tanyukang <tanyukang888@163.com>
2022-05-30 17:41:19 +0800

    1

--- src/views/users/userList.vue
+++ src/views/users/userList.vue
@@ -1,8 +1,6 @@
 <template>
-  <!-- <el-card class="box-card">
-    hahah 
-  </el-card> -->
-  <ElForm ref="formRef" :inline="true" :model="form" :rules="rules">
+  <p></p>
+  <ElForm ref="formRef" :inline="true" :model="form">
     <ElFormItem prop="userName">
       <el-input v-model="form.userName" placeholder="请输入用户名" clearable />
     </ElFormItem>
@@ -16,75 +14,94 @@
     </ElFormItem>
   </ElForm>
 
-  <el-table :data="tableData" style="width: 100%">
+  <el-table :data="pageData.tableData" style="width: 100%">
     <el-table-column prop="date" label="Date" width="180" />
     <el-table-column prop="name" label="Name" width="180" />
     <el-table-column prop="address" label="Address" />
     <el-table-column label="操作">
       <template #default="scope">
         <el-button size="small">Edit</el-button>
-        <el-button size="small" type="danger" @click="handleDelete(scope.$index, scope.row)">Delete</el-button>
+        <el-button
+          size="small"
+          type="danger"
+          @click="handleDelete(scope.$index, scope.row)"
+          >Delete</el-button
+        >
       </template>
     </el-table-column>
   </el-table>
-  <el-pagination background layout="prev, pager, next" :current-page="paginate.current" :page-size="paginate.pageSize"
-    :total="paginate.total" hide-on-single-page />
+  <ElPagination
+    background
+    layout="total, sizes, prev, pager, next, jumper"
+    v-model:currentPage="pageData.tableData.currentPage"
+    :pageSize="pageData.tableData.pageSize"
+    :total="pageData.tableData.total"
+  />
 </template>
 
 <script setup>
-import { ElCard, ElForm, ElFormItem, ElTable, ElTableColumn, ElInput, ElButton, ElPagination } from 'element-plus';
-import { reactive } from 'vue';
-const tableData = reactive([]);
+import {
+  ElCard,
+  ElForm,
+  ElFormItem,
+  ElTable,
+  ElTableColumn,
+  ElInput,
+  ElButton,
+  ElPagination,
+} from "element-plus";
+import { reactive, ref } from "vue";
 const form = reactive({
   userName: "",
-  iphone: ""
-})
-
-const paginate = reactive({
-  total: 0,
-  current: 1,
-  pageSize: 10
-})
+  iphone: "",
+});
+const pageData = reactive({
+  tableData: [],
+  paginate: {
+    currentPage: 1,
+    pageSize: 2,
+    total: 0,
+  },
+});
 
 const search = () => {
-  // tableData = JSON.parse(JSON.stringify(data))
-  // const data = 
-  let data = [
+  pageData.tableData = [
     {
-      date: '2016-05-03',
-      name: 'Tom',
-      address: 'No. 189, Grove St, Los Angeles',
+      date: "2016-05-03",
+      name: "Tom",
+      address: "No. 189, Grove St, Los Angeles",
     },
     {
-      date: '2016-05-02',
-      name: 'Tom',
-      address: 'No. 189, Grove St, Los Angeles',
+      date: "2016-05-02",
+      name: "Tom",
+      address: "No. 189, Grove St, Los Angeles",
     },
     {
-      date: '2016-05-04',
-      name: 'Tom',
-      address: 'No. 189, Grove St, Los Angeles',
+      date: "2016-05-04",
+      name: "Tom",
+      address: "No. 189, Grove St, Los Angeles",
     },
     {
-      date: '2016-05-01',
-      name: 'Tom',
-      address: 'No. 189, Grove St, Los Angeles',
+      date: "2016-05-01",
+      name: "Tom",
+      address: "No. 189, Grove St, Los Angeles",
     },
-  ]
-  console.log(tableData)
-  tableData.value = tableData.concat(data)
-  paginate.total = tableData.length;
-  paginate.current = 1;
-}
+  ];
+  pageData.paginate.total = pageData.tableData.length;
+  pageData.paginate.currentPage = 1;
+};
 const addUser = () => {
-  tableData.push({
-    date: '2016-05-01',
-    name: 'Tom',
-    address: 'No. 189, Grove St, Los Angeles',
-  })
-}
+  pageData.tableData.push({
+    date: "2016-05-01",
+    name: "Tom",
+    address: "No. 189, Grove St, Los Angeles",
+  });
+  pageData.paginate.total = pageData.tableData.length;
+  pageData.paginate.currentPage = 1;
+};
 const handleDelete = (index, row) => {
-  tableData.splice(index, 1)
-}
-
+  pageData.tableData.splice(index, 1);
+  pageData.paginate.total = pageData.tableData.length;
+  pageData.paginate.currentPage = 1;
+};
 </script>
