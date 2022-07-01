56490699ef6645f9425ccca8a2161e18f60fa519
tanyukang <tanyukang888@163.com>
2022-07-01 17:41:11 +0800

    xiu

--- src/views/echarts/echarts1.vue
+++ src/views/echarts/echarts1.vue
@@ -19,28 +19,32 @@ function fun1() {
   console.log("script fun1");
 }
 function echartInit() {
-  const myChart = echarts.init(document.getElementById("echarts1"));
-  const option = {
-    title: {
-      text: "ECharts 入门示例",
-    },
-    tooltip: {},
-    legend: {
-      data: ["销量"],
-    },
-    xAxis: {
-      data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"],
-    },
-    yAxis: {},
-    series: [
-      {
-        name: "销量",
-        type: "bar",
-        data: [5, 20, 36, 10, 10, 20],
+  let ele: HTMLElement | null = document.getElementById("echarts1");
+  if (ele) {
+    const myChart = echarts.init(ele);
+    const option = {
+      title: {
+        text: "ECharts 入门示例",
       },
-    ],
-  };
-
-  myChart.setOption(option);
+      tooltip: {},
+      legend: {
+        data: ["销量"],
+      },
+      xAxis: {
+        data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"],
+      },
+      yAxis: {},
+      series: [
+        {
+          name: "销量",
+          type: "bar",
+          data: [5, 20, 36, 10, 10, 20],
+        },
+      ],
+    };
+    myChart.setOption(option);
+  }
 }
 </script>
+
+<!-- https://juejin.cn/post/7110225928067743751 -->
